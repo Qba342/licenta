4593898844e13ec933e798956841744ed7fbdde5
Qba342 <iulianhalmagean@gmail.com>
2021-03-18 03:58:34 +0200

    Am refacut proiectul astfel incat acum implementam clase

--- Blog_Scrapper/scrapper.py
+++ Blog_Scrapper/scrapper.py
@@ -33,65 +33,70 @@ def RemoveComFromString(string):
     sep = '#'
     return (string.split(sep, 1)[0])
 
+class BlogScrapper:
+    def __init__(self,base_url,older_posts):
+        self.base_url=base_url
+        self.older_search=older_posts
+        self.listaBloguri=[]
 
-def GetBlogsFromPage(base_url, url):
-    listaBloguriPePagina = []
-    html_content = requests.get(url).text
-    soup = BeautifulSoup(html_content, "html.parser")
-    for link in soup.find_all("a"):
-        redirect = xstr(link.get("href"))
-        print(redirect)
-        if (is_in_same_domain(get_domain(base_url), redirect)):  # verificam daca este in acelasi domeniu
-            listaBloguriPePagina.append(RemoveComFromString(redirect))
-    for blog in listaBloguriPePagina:
-        if (blog.find("?") != -1):
-            listaBloguriPePagina.remove(blog)
-    return listaBloguriPePagina
+    def GetBlogsFromPage(self):
 
-
-def GetPagesList(base_url, nrPagini):
-    listaPaginiFinala = []
-    for i in range(nrPagini):
-        url = base_url + "page/" + str(i + 1)
-        listaPaginiFinala = listaPaginiFinala + GetBlogsFromPage(base_url, url)
-        # time.sleep(2)
-    listaPaginiFinala = list(dict.fromkeys(listaPaginiFinala))
-    return listaPaginiFinala
+        html_content = requests.get(self.base_url).text
+        soup = BeautifulSoup(html_content, "html.parser")
+        for link in soup.find_all("a"):
+            redirect = xstr(link.get("href"))
+            print(redirect)
+            if (is_in_same_domain(get_domain(self.base_url), redirect)):  # verificam daca este in acelasi domeniu
+                self.listaBloguri.append(RemoveComFromString(redirect))
+        for blog in self.listaBloguri:
+            if (blog.find("?") != -1):
+                self.listaBloguri.remove(blog)
 
 
-def download(url, downloadSource):
-    r = requests.get(url, allow_redirects=True)
-    # print(url+": "+r.headers.get('content-type'))
-    # if(url[-1]=='/'):
-    #    url=url[:-1]
-    # name=url.rsplit("/",1)[1]
 
-    os.makedirs(os.path.dirname(downloadSource), exist_ok=True)
-    with open(downloadSource, 'wb') as f:
-        f.write(r.content)
+    def GetPagesList(self, nrPagini):
+        listaPaginiFinala = []
+        for i in range(nrPagini):
+            url = self.base_url + "page/" + str(i + 1)
+            listaPaginiFinala = listaPaginiFinala + self.GetBlogsFromPage(self.base_url, url)
+            # time.sleep(2)
+        listaPaginiFinala = list(dict.fromkeys(listaPaginiFinala))
+        return listaPaginiFinala
 
 
-def DownloadResources(base_url, older_search):
-    base_download = base_url.split("/", 2)[-1].rsplit(".", 1)[0]
-    if (base_download.find(".")):
-        base_download = base_download.rsplit(".", 1)[-1]
-    DownloadAdress = "downloads/" + base_download + "/"
-    if (older_search == True):  # daca dorim o cautare printre paginile mai vechi
-        listaPagini = GetPagesList(base_url, 1)  # aici trebuie sa setam numarul paginilor
+    def download(self,url, downloadSource):
+        r = requests.get(url, allow_redirects=True)
 
-        for pagina in listaPagini:
-            #  ListaImagini=GetImages(base_url,pagina)
-            md5 = hashlib.md5(pagina.encode()).hexdigest()
-            DownloadAdress2 = DownloadAdress + md5  # pentru a tine evidenta
-            download(pagina, DownloadAdress2)
-    else:
-        listaBloguri = GetBlogsFromPage(base_url, base_url)
-        for blog in listaBloguri:
-            md5 = hashlib.md5(blog.encode()).hexdigest()
-            DownloadAdress2 = DownloadAdress + md5  # pentru a tine evidenta
-            download(blog, DownloadAdress2)
+        os.makedirs(os.path.dirname(downloadSource), exist_ok=True)
+        with open(downloadSource, 'wb') as f:
+            f.write(r.content)
+
+
+    def DownloadResources(self):
+        base_download = self.base_url.split("/", 2)[-1].rsplit(".", 1)[0]
+        if (base_download.find(".")):
+            base_download = base_download.rsplit(".", 1)[-1]
+        DownloadAdress = "downloads/" + base_download + "/"
+        if (self.older_search == True):  # daca dorim o cautare printre paginile mai vechi
+            listaPagini = self.GetPagesList(self.base_url, 1)  # aici trebuie sa setam numarul paginilor
+
+            for pagina in listaPagini:
+                #  ListaImagini=GetImages(base_url,pagina)
+                md5 = hashlib.md5(pagina.encode()).hexdigest()
+                DownloadAdress2 = DownloadAdress + md5  # pentru a tine evidenta
+                self.download(pagina, DownloadAdress2)
+        else:
+            self.GetBlogsFromPage()
+
+            for blog in self.listaBloguri:
+                md5 = hashlib.md5(blog.encode()).hexdigest()
+                DownloadAdress2 = DownloadAdress + md5  # pentru a tine evidenta
+                self.download(blog, DownloadAdress2)
 
     # GetImages(base_url,listaPagini[10])# vom prelucrea imaginile astfel incat sa preluam scrisul din ele
 
 
-DownloadResources("https://thehackernews.com/", False)  # vom pune True daca vrem sa cautam si in postari mai vechi
+#DownloadResources("https://thehackernews.com/", False)  # vom pune True daca vrem sa cautam si in postari mai vechi
+
+#scrap=BlogScrapper("https://thehackernews.com/", False)
+#scrap.DownloadResources()
--- Relevant_content_picker/FilterHtml.py
+++ Relevant_content_picker/FilterHtml.py
@@ -6,66 +6,76 @@ def listToString(str):
     return(str1.join(str))
 
 
-
-def getClassList(source):
-    listaClase=[]#lista in care vom stoca toate clasele
-
-    soup=BeautifulSoup(open(source),'html.parser')
-    divs=soup.find_all("div",class_=True)
-    for div in divs:
-        listaClase.append(listToString(div['class']))
-    return listaClase
+class FilterHtml:
 
 
+    def __init__(self,source):
+        self.source=source
+        self.soup=BeautifulSoup(open(self.source),'html.parser')
+        self.listaClase=[]
+        self.listaImagini=[]
 
-def getBestDiv(source,divList):
-    soup=BeautifulSoup(open(source),'html.parser')
-    tupleList=[]
+    def setClassList(self):
 
-    for div in divList:
-        res=soup.find("div",{"class":div})
-        children=res.findChildren("p",recursive=False)
-        tupleList.append((len(children),div)) #creeam o lista cu copii care contin cele mai multe paragrafe
-    tupleList.sort(key=lambda tup: tup[0],reverse=True)#sortam lista descrescator
-    bestDiv=tupleList[0][1]
-   # res = soup.find("div", {"class": tupleList[1][1]})
-    #print(res.text)
-    longFinal=0
 
-    if(tupleList[0][0]==tupleList[1][0]):#verificam daca primele doua intrari au valori egale (in contextul in care art este scris cu un singur paragraf)
-        n=0
-        while(tupleList[n][0]==tupleList[n+1][0]):
-            res = soup.find("div", {"class": tupleList[n][1]})
-            long=len(res.text)
-            if(long>longFinal):
-                longFinal=long
-                bestDiv=tupleList[n][1]
-            n=n+1
-
-    return (bestDiv)
+        divs= self.soup.find_all("div", class_=True)
+        for div in divs:
+            self.listaClase.append(listToString(div['class']))
 
 
 
-def getUsefullResources(source,div,type):
-    listaImagini = []
-    soup = BeautifulSoup(open(source), "html.parser")
-    divToSearch=soup.find("div",{"class":div})
-    images=divToSearch.find_all(type)
-    for image in images:
-        listaImagini.append(image['src'])
-    return listaImagini
+    def setBestDiv(self):
+
+        tupleList=[]
+
+        for div in self.listaClase:
+            res= self.soup.find("div", {"class":div})
+            children=res.findChildren("p",recursive=False)
+            tupleList.append((len(children),div)) #creeam o lista cu copii care contin cele mai multe paragrafe
+        tupleList.sort(key=lambda tup: tup[0],reverse=True)#sortam lista descrescator
+        bestDiv=tupleList[0][1]
+       # res = soup.find("div", {"class": tupleList[1][1]})
+        #print(res.text)
+        longFinal=0
+
+        if(tupleList[0][0]==tupleList[1][0]):#verificam daca primele doua intrari au valori egale (in contextul in care art este scris cu un singur paragraf)
+            n=0
+            while(tupleList[n][0]==tupleList[n+1][0]):
+                res = self.soup.find("div", {"class": tupleList[n][1]})
+                long=len(res.text)
+                if(long>longFinal):
+                    longFinal=long
+                    bestDiv=tupleList[n][1]
+                n=n+1
+
+        self.div=bestDiv
 
 
-def extractor(blog_File):
-    blogList=getClassList(blog_File)
-   # difs=differences(blogList,baseList)
-    div=getBestDiv(blog_File,blogList)
-    getUsefullResources(blog_File,div,"img")
-    getUsefullResources(blog_File, div, "img")
-    #TODO: Trebuie sa procesam si fisierele de tip pdf. Acestea se gasesc folosind a href="blabla.pdf"
-    #TODO: O sa incepem sa facem parsarea imaginilor
-    #print(div)
+
+    def extractUsefullResources(self, type):
+
+        divToSearch= self.soup.find("div", {"class": self.div})
+        images=divToSearch.find_all(type)
+        for image in images:
+            self.listaImagini.append(image['src'])
+
+    def print_content(self):
+        to_print= self.soup.find("div", {"class": self.div})
+        print(to_print.text)
+
+    def extract(self):
+        self.setClassList()
+       # difs=differences(blogList,baseList)
+        self.setBestDiv()
+        self.extractUsefullResources("img")
+
+        #TODO: Trebuie sa procesam si fisierele de tip pdf. Acestea se gasesc folosind a href="blabla.pdf"
+        #TODO: O sa incepem sa facem parsarea imaginilor
+        #print(div)
 
 
 #extractor("C:/Users/Iulian/Desktop/Proiect Licenta/Blog_Scrapper/downloads/krebsonsecurity/7a3c42a708726ac7534008355eae270f")
-extractor("C:/Users/Iulian/Desktop/Proiect Licenta/Blog_Scrapper/downloads/thehackernews/521bf76645b5cda738822771799e4dc3")
+#extractor("C:/Users/Iulian/Desktop/Proiect Licenta/Blog_Scrapper/downloads/thehackernews/65bc8f398951089f8aa2938c4634a534")
+filter=FilterHtml("C:/Users/Iulian/Desktop/Proiect Licenta/Blog_Scrapper/downloads/thehackernews/65bc8f398951089f8aa2938c4634a534")
+filter.extract()
+filter.print_content()
